ddf6bb4cfdfdb361fdf6a91c8ad800c97126b8c3
Ellen <eol50559@gmail.com>
2020-12-17 00:09:18 +0000

    topic 10 - passport

--- movies-api/index.js
+++ movies-api/index.js
@@ -7,7 +7,7 @@ import {loadUsers} from './seedData';
 import usersRouter from './api/users';
 import genresRouter from './api/genres';
 import session from 'express-session';
-import authenticate from './authenicate';
+import passport from './authenicate';
 
 dotenv.config();
 
@@ -33,12 +33,13 @@ if(process.env.SEED_DB){
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(passport.initialize());
 app.use(session({
   secret: "ilikecake",
   resave : true,
   saveUninitialized : true
 }));
-app.use('/api/movies', authenticate, moviesRouter);
+app.use('/api/movies', passport.authenticate('jwt', {session:false}), moviesRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/genres', genresRouter);
 app.use(errHandler);
